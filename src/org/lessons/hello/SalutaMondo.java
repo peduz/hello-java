5710325f72edb94240648e9ee1b367f9c78213c4
Giuseppe Pedulla <gpedulla@ITPRESENT.onmicrosoft.com>
2024-09-12 20:17:32 +0200

    Cambio di saluto con le maiuscole

--- src/org/lessons/hello/SalutaMondo.java
+++ src/org/lessons/hello/SalutaMondo.java
@@ -3,6 +3,6 @@ package org.lessons.hello;
 public class SalutaMondo {
 	//Metodo main
 	public static void main(String[] args) {
-		System.out.println("Ciao pippo e pluto e paperino!");
+		System.out.println("Ciao Pippo e Pluto e Paperino!");
 	}
 }
